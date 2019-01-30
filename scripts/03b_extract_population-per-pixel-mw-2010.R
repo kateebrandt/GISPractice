f0e1d3a55a265b68063d76203381c8ad082d3804
Brandt <kebrandt@ad.unc.edu>
2019-01-30 13:45:43 -0500

    2010 popden calc

--- scripts/03b_extract_population-per-pixel-mw-2010.R
+++ scripts/03b_extract_population-per-pixel-mw-2010.R
@@ -13,10 +13,10 @@ popden <- raster("data/MWI_pph_2010_adj_v2.tif")
 hh_geo <- readOGR(dsn = "./data",layer = "mw10_hh_geo_sp_wgs84")
 
 ## popden .tif needs to have projection changed
-crs(popden) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
+hh_geo_proj <- hh_geo %>% spTransform(crs(popden))
 
 ## check if CRS is same for both
-identicalCRS(hh_geo,popden)
+identicalCRS(hh_geo_proj,popden)
 
 #extract and mutate popden data to hh geo
 #hh_geo@data %<>% mutate(access_to_city15_nobuff = raster::extract(popden, hh_geo),
@@ -30,15 +30,17 @@ identicalCRS(hh_geo,popden)
 ### buffering takes a long time
 # run over small set
 
-  ## Jan 30 - this isn't working. Just putting out NA's for popden buffer column
-hh_geo@data %<>% mutate(popden_buff10k = NA)
-for(i in seq(1,nrow(hh_geo),by=102)){
-  hh_geo@data[i:(i+101),"popden_buff10k"] <-
-    raster::extract(popden, hh_geo[i:(i+101),],
+  ## Jan 30 - had to calculate inefficiently :(
+hh_geo_proj@data %<>% mutate(popden_buff10k = NA)
+#for(i in seq(1,nrow(hh_geo),by=102)){
+  for(i in 1:12271){
+  hh_geo_proj@data[i,"popden_buff10k"] <-
+    raster::extract(popden, hh_geo[i,],
                     buffer = 10000, fun = sum)
   print(i)
 }
 
+hh_geo_proj@data$popden_buff10k
 ## divide popden by 10 km sq
 hh_geo@data %<>% mutate(popden_buff10k = popden_buff10k/(100*pi))
 
