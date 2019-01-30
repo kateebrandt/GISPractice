a60a935e4ece96b2188c18bce7e66964d0189978
Kate Brandt <kbrandt.1744@gmail.com>
2019-01-30 09:05:35 -0500

    updated: first draft was erroneously using population data instead of access data.

--- scripts/02b_extract_accessibilitytoCities_MW10.R
+++ scripts/02b_extract_accessibilitytoCities_MW10.R
@@ -7,14 +7,10 @@ library(magrittr)
 library(tidyverse)
 
 ## load access raster - Use 2010 data for 2010 survey
-access <- raster("data/mw100m_population/MWI_pph_2010_adj_v2.tif")
+access <- raster("data/accessibility_to_cities_2015_v1.0.tif")
 
 ## load malawi hh point shp
 hh_geo <- readOGR(dsn = "./data",layer = "mw10_hh_geo_sp_wgs84")
-
-
-## Jan 30 - NOTE: Access is has different projection. Changed here:
-crs(access) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
 
 ## check if CRS is same for both
   identicalCRS(hh_geo,access)
@@ -28,7 +24,7 @@ crs(access) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
 #                                                                     ))
 
 ##store data on drive
-write_rds(x = hh_geo,path="data/hhgeo_w_calculated_access.rds",compress = "gz")
+write_rds(x = hh_geo,path="data/hhgeo10_w_calculated_access.rds",compress = "gz")
 
 ### buffering takes a long time
 # run over small set
@@ -40,6 +36,8 @@ for(i in seq(1,nrow(hh_geo),by=102)){
   print(i)
 }
 
-##store data on drive
-write_rds(x = hh_geo,path="data/hhgeo10_w_access.rds",compress = "gz")
+access2cities10 <- raster::extract(access, hh_geo)
 
+## Check output
+hh_geo@data[1:20,]
+summary(hh_geo@data$access_to_city15_buff10k)
