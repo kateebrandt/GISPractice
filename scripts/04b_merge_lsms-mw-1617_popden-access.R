21133f04a9392c824b8a72adfcac26d31ce88af1
Brandt <kebrandt@ad.unc.edu>
2019-02-20 14:02:00 -0500

    finished script

--- scripts/04b_merge_lsms-mw-1617_popden-access.R
+++ scripts/04b_merge_lsms-mw-1617_popden-access.R
@@ -6,11 +6,13 @@ library(dplyr)
 
 # read files to be merged into data frame format
 
-access1617 <- data.frame(readRDS("./data/hhgeo_w_calculated_access.rds"))
+access <- data.frame(readRDS("./data/hhgeo_w_calculated_access.rds"))
 popden <- data.frame(readRDS("./data/hhgeo_w_popden10k (1).rds"))
 
 # copy popden10k column to main data frame
 hhgeo1617_mw_lsms_mergedvar <- merge(access, popden) 
+# remove unwanted columns
+hhgeo1617_mw_lsms_mergedvar <- hhgeo1617_mw_lsms_mergedvar[, -c(5,6,7)]
 # write to csv for upload
 write.csv(hhgeo1617_mw_lsms_mergedvar, file = "hhgeo1617_mw_lsms.csv", row.names = FALSE)
 
