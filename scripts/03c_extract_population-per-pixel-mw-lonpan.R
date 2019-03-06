5d63ad80959f911a442becfd1984b29eca3d7df5
Brandt <kebrandt@ad.unc.edu>
2019-03-06 18:36:46 -0500

    completed the re-do of panel datasets

--- scripts/03c_extract_population-per-pixel-mw-lonpan.R
+++ scripts/03c_extract_population-per-pixel-mw-lonpan.R
@@ -1,19 +1,19 @@
 ## KB
-## February 4 2019
+## March 6 2019
 
 library(raster)
 library(rgdal)
 library(magrittr)
 library(tidyverse)
 
-## 2013 data needs average between 2010 popden and 2015 popden
+## Remember: 2013 data needs average between 2010 popden and 2015 popden
 ######################################################
-# PART 1 - 2010 Popden
+# PART 1 - 2010 Popden for YEAR 1
 ## load access raster
 popden <- raster("data/MWI_pph_2010_adj_v2.tif")
 
 ## load malawi hh point shp
-hh_geo <- readOGR(dsn = "./data",layer = "mwlonpan_hh_geo_sp_wgs84")
+hh_geo <- readOGR(dsn = "./data",layer = "mwlpy1_hh_geo_sp_wgs84")
 
 ## popden .tif needs to have projection changed
 hh_geo_proj <- hh_geo %>% spTransform(crs(popden))
@@ -21,43 +21,30 @@ hh_geo_proj <- hh_geo %>% spTransform(crs(popden))
 ## check if CRS is same for both
 identicalCRS(hh_geo_proj,popden)
 
-#extract and mutate popden data to hh geo
-#hh_geo@data %<>% mutate(access_to_city15_nobuff = raster::extract(popden, hh_geo),
-#                        access_to_city15_buff10k = raster::extract(popden, hh_geo,
-#                                                                   buffer = 10000,
-#                                                                   fun = mean,
-#                                                                     ))
-
-
-
-### buffering takes a long time
-# run over small set
-
-## Feb 4 - had to calculate inefficiently :(
-hh_geo_proj@data %<>% mutate(popden_buff10k = NA)
+## Extract 2010 popden to YEAR 1
+hh_geo_proj@data %<>% mutate(popden_buff10k_2010 = NA)
 #for(i in seq(1,nrow(hh_geo),by=102)){
 for(i in 1:1619){
-  hh_geo_proj@data[i,"popden_buff10k"] <-
+  hh_geo_proj@data[i,"popden_buff10k_2010"] <-
     raster::extract(popden, hh_geo[i,],
                     buffer = 10000, fun = sum)
   print(i)
 }
 
-hh_geo_proj@data$popden_buff10k
+hh_geo_proj@data$popden_buff10k_2010
 ## divide popden by 10 km sq
-hh_geo_proj@data %<>% mutate(popden_buff10k = popden_buff10k/(100*pi))
+hh_geo_proj@data %<>% mutate(popden_buff10k_2010 = popden_buff10k_2010/(100*pi))
 
 ##store data on drive
-write_rds(x = hh_geo_proj,path="data/hhgeolonpan_w_popden10k.rds",compress = "gz")
+write_rds(x = hh_geo_proj,path="data/hhgeolpy1_w_popden10k.rds",compress = "gz")
 
 #####################################################################################
-## PART 2 - 2015 Popden
+## PART 2 - 2010 and 2015 popden, averaged, for YEAR 2 (2013)
 
-# load 2015 raster
-popden <- raster("data/Malawi 100m Population (3)/MWI_pph_2015_adj_v2.tif")
+# No need to load popden, 2010 already loaded
 
 ## load malawi hh point shp
-hh_geo <- readOGR(dsn = "./data",layer = "mwlonpan_hh_geo_sp_wgs84")
+hh_geo <- readOGR(dsn = "./data",layer = "mwlpy2_hh_geo_sp_wgs84")
 
 ## popden .tif needs to have projection changed
 hh_geo_proj <- hh_geo %>% spTransform(crs(popden))
@@ -65,48 +52,76 @@ hh_geo_proj <- hh_geo %>% spTransform(crs(popden))
 ## check if CRS is same for both
 identicalCRS(hh_geo_proj,popden)
 
-#extract and mutate popden data to hh geo
-#hh_geo@data %<>% mutate(access_to_city15_nobuff = raster::extract(popden, hh_geo),
-#                        access_to_city15_buff10k = raster::extract(popden, hh_geo,
-#                                                                   buffer = 10000,
-#                                                                   fun = mean,
-#                                                                     ))
+## PART 2A
+## Extract 2010 popden to YEAR 2 
+hh_geo_proj@data %<>% mutate(popden_buff10k_2010 = NA)
 
-
-
-### buffering takes a long time
-# run over small set
-
-## Feb 4 - had to calculate inefficiently :(
-hh_geo_proj@data %<>% mutate(popden_buff10k_15 = NA)
-#for(i in seq(1,nrow(hh_geo),by=102)){
-for(i in 1:1619){
-  hh_geo_proj@data[i,"popden_buff10k_15"] <-
+for(i in 1:1990){
+  hh_geo_proj@data[i,"popden_buff10k_2010"] <-
     raster::extract(popden, hh_geo[i,],
                     buffer = 10000, fun = sum)
   print(i)
 }
 
-hh_geo_proj@data$popden_buff10k_15
+hh_geo_proj@data$popden_buff10k_2010
 ## divide popden by 10 km sq
-hh_geo_proj@data %<>% mutate(popden_buff10k_15 = popden_buff10k_15/(100*pi))
+hh_geo_proj@data %<>% mutate(popden_buff10k_2010 = popden_buff10k_2010/(100*pi))
+
+## PART 2B
+# Popden now 2015 data
+popden <- raster("data/Malawi 100m Population (3)/MWI_pph_2015_adj_v2.tif")
+
+## check if CRS is same for both
+identicalCRS(hh_geo_proj,popden)
+
+## Extract 2015 popden to YEAR 2 
+hh_geo_proj@data %<>% mutate(popden_buff10k_2015 = NA)
+
+for(i in 1:1990){
+  hh_geo_proj@data[i,"popden_buff10k_2015"] <-
+    raster::extract(popden, hh_geo[i,],
+                    buffer = 10000, fun = sum)
+  print(i)
+}
+
+hh_geo_proj@data$popden_buff10k_2015
+## divide popden by 10 km sq
+hh_geo_proj@data %<>% mutate(popden_buff10k_2015 = popden_buff10k_2015/(100*pi))
+
+
+## PART 2C
+## Create average of 2010 and 2015 for "2013" popden
+hh_geo_proj@data %<>% mutate(popden_buff10k_2013 = (popden_buff10k_2010 + popden_buff10k_2015)/2)
 
 ##store data on drive
-write_rds(x = hh_geo_proj,path="data/hhgeolonpan_w_popden10k_15.rds",compress = "gz")
+write_rds(x = hh_geo_proj,path="data/hhgeolpy2_w_popden10k_10_13_15.rds",compress = "gz")
 
 #########################################################################################
-## PART 3 - Merge data frames and average the two years' values
+## PART 3 - YEAR 3
+# Access already loaded as 2015 data
 
-popden2010 <- readRDS("./data/hhgeolonpan_w_popden10k.rds")
-popden2015 <- readRDS("./data/hhgeolonpan_w_popden10k_15.rds")
+## load malawi hh point shp
+hh_geo <- readOGR(dsn = "./data",layer = "mwlpy3_hh_geo_sp_wgs84")
 
-popden2010 <- popden2010@data
-popden2015 <- popden2015@data
+## popden .tif needs to have projection changed
+hh_geo_proj <- hh_geo %>% spTransform(crs(popden))
 
-popdens2 <- merge(popden2010, popden2015)
+## check if CRS is same for both
+identicalCRS(hh_geo_proj,popden)
 
-popdens2 <- mutate(popdens2, popden10k_av_2013 = (popden_buff10k + popden_buff10k_15)/2)
+## Extract 2015 popden to YEAR 3
+hh_geo_proj@data %<>% mutate(popden_buff10k_2015 = NA)
 
-write_rds(x = popdens2, path="data/hhgeolonpan_w_popden10k_comp.rds",compress = "gz")
+for(i in 1:2503){
+  hh_geo_proj@data[i,"popden_buff10k_2015"] <-
+    raster::extract(popden, hh_geo[i,],
+                    buffer = 10000, fun = sum)
+  print(i)
+}
 
+hh_geo_proj@data$popden_buff10k_2015
+## divide popden by 10 km sq
+hh_geo_proj@data %<>% mutate(popden_buff10k_2015 = popden_buff10k_2015/(100*pi))
 
+##store data on drive
+write_rds(x = hh_geo_proj,path="data/hhgeolpy3_w_popden10k_15.rds",compress = "gz")
--- scripts/04c_merge_lsms-mw-lonpan_popden-access.R
+++ scripts/04c_merge_lsms-mw-lonpan_popden-access.R
@@ -5,14 +5,48 @@
 library(dplyr)
 library(base)
 library(plyr)
-# read files to be merged into data frame format
 
-accesslp <- data.frame(readRDS("./data/hhgeolonpan_w_calculated_access.rds"))
-popden <- data.frame(readRDS("./data/hhgeolonpan_w_popden10k_comp.rds"))
+## YEAR 1 - 2010 Geovar merge
+
+# read files to be merged into data frame format
+access <- data.frame(readRDS("./data/hhgeolpy1_w_calculated_access.rds"))
+popden <- data.frame(readRDS("./data/hhgeolpy1_w_popden10k.rds"))
 
 # copy popden10k column to main data frame
-hhgeolp_mw_lsms_mergedvar <- left_join(access, popden, by = "case_id") 
+hhgeolpy1 <- left_join(access, popden, by = "case_id") 
 # remove repeated columns
-hhgeolp_mw_lsms <- hhgeolp_mw_lsms_mergedvar[, -c(6,7,8,9,10,11)]
+hhgeolpy1 <- hhgeolpy1[, c(1:7,12)]
 # write to csv for upload
-write.csv(hhgeolp_mw_lsms, file = "hhgeolonpan_mw_lsms.csv", row.names = FALSE)
+write.csv(hhgeolpy1, file = "hhgeolpy1_mw_lsms.csv", row.names = FALSE)
+
+#############################################################################
+## YEAR 2 - 2013 Geovar merge
+
+# read files to be merged into data frame format
+access <- data.frame(readRDS("./data/hhgeolpy2_w_calculated_access.rds"))
+popden <- data.frame(readRDS("./data/hhgeolpy2_w_popden10k_10_13_15.rds"))
+# subset needed columns for merge
+access <- access[,c(1,9)]
+
+# copy popden10k column to main data frame
+hhgeolpy2 <- left_join(access, popden, by = "y2_hhid") 
+hhgeolpy2 <- hhgeolpy2[,-c(13:15)]
+# write to csv for upload
+write.csv(hhgeolpy2, file = "hhgeolpy2_mw_lsms.csv", row.names = FALSE)
+
+
+#############################################################################
+## YEAR 3 - 2016-17 Geovar merge
+
+# read files to be merged into data frame format
+access <- data.frame(readRDS("./data/hhgeolpy3_w_calculated_access.rds"))
+popden <- data.frame(readRDS("./data/hhgeolpy3_w_popden10k_15.rds"))
+# subset needed columns for merge
+access <- access[,c(1,12)]
+
+# copy popden10k column to main data frame
+hhgeolpy3 <- right_join(access, popden, by = "y3_hhid") 
+# remove repeated columns
+hhgeolpy3 <- hhgeolpy3[, -c(14:16)]
+# write to csv for upload
+write.csv(hhgeolpy3, file = "hhgeolpy3_mw_lsms.csv", row.names = FALSE)
